275992efd31ddfb707af2d003e0184f5b8704723
Yakimov Grigory <73303452+Grigory98@users.noreply.github.com>
2021-10-26 09:46:02 +0400

    Create CyclingRotation.go

---
+++ Cycling Rotation/CyclingRotation.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A = [5]int{3, 8, 9, 7, 6}
+	fmt.Println(Solution(A[:], 3))
+}
+
+func Solution(A []int, K int) []int {
+	var temp int
+	
+	for i:=0; i < K; i++ {
+		temp = A[len(A)-1]
+		
+		for j:=len(A)-1; j>0; j-- {
+			A[j] = A[j-1]
+		}
+		A[0] = temp
+	}
+	return A
+}
