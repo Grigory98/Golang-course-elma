37434916e01fe9c6ed81d67c6cca8cee2e551f06
Yakimov Grigory <73303452+Grigory98@users.noreply.github.com>
2021-10-27 14:55:56 +0400

    Update Task3.go

--- Entries in array/Task3.go
+++ Entries in array/Task3.go
@@ -9,17 +9,21 @@ func main() {
 
 func Solution(A []int) int {
 	var temp int
-
-	for i := 0; i < len(A); i++ {
-
-		if i < len(A)-2 {
-
-			if A[i] != A[i+2] {
-				temp = A[i+2]
+	size := len(A)
+	counter := 0
+	
+	for i:=0; i < size; i++ {
+		counter = 0
+		for j:=0; j < size; j++ {
+			if A[i] == A[j] {
+				counter = counter + 1
 			}
-
 		}
-
+		
+		if counter % 2 != 0 {
+			temp = A[i]
+		}
+		
 	}
 	return temp
 }
