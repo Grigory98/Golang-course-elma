5d23f1249f4ccfa9158df8b57f49c1d1355323ef
Yakimov Grigory <73303452+Grigory98@users.noreply.github.com>
2021-11-01 15:16:51 +0400

    Update Task4.go

--- Sequence validation/Task4.go
+++ Sequence validation/Task4.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var arr = []int{4, 1, 3, 2}
+	var arr = []int{6, 3, 5, 4}
 	fmt.Println(Solution(arr))
 }
 
 func Solution(A []int) int {
 	var temp int
 	temp = 0
-
+	min := findMin(A)
 	for i := 1; i <= len(A); i++ {
 
-		if !findElem(A, i) {
+		if !findElem(A, min) {
 			temp = temp + 1
 		}
+		min = min + 1
 	}
 
 	if temp > 0 {
@@ -24,9 +25,7 @@ func Solution(A []int) int {
 	return 1
 }
 
-/**
-* Проверяет вхождения в массив
-**/
+//Проверяет вхождения в массив
 func findElem(A []int, x int) bool {
 	for _, n := range A {
 		if x == n {
@@ -34,4 +33,16 @@ func findElem(A []int, x int) bool {
 		}
 	}
 	return false
+}
+
+//Находит минимальное значение в массиве
+func findMin(values []int) int {
+	min := values[0]
+
+	for _, v := range values {
+		if v < min {
+			min = v
+		}
+	}
+	return min
 }
